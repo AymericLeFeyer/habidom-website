d7a7254b1931e24085f05b39d51b5a61286c304c
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2020-09-01 15:02:33 +0200

    send mail

--- assets/js/ajax-contact.js
+++ assets/js/ajax-contact.js
@@ -7,42 +7,79 @@ $(function() {
 	var formMessages = $('.form-message');
 
 	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
+	$(form).on('submit', function(e) {
 		// Stop the browser from submitting the form.
 		e.preventDefault();
 
 		// Serialize the form data.
-		var formData = $(form).serialize();
+		var formData = {
+			name: form.find('input[name="name"]').val(),
+			mail: form.find('input[name="email"]').val(),
+			society: form.find('input[name="society"]').val(),
+			phone: form.find('input[name="phone"]').val(),
+			message: form.find('textarea[name="message"]').val(),
+		}
 
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text(response);
-
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
+		if (formData.name == "") {
 			$(formMessages).removeClass('success');
 			$(formMessages).addClass('error');
+			$(formMessages).text('Veuillez renseigner un nom valide');
+		} else if (formData.mail == "") {
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+			$(formMessages).text('Veuillez renseigner un mail valide');
+		} else if (formData.phone == "") {
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+			$(formMessages).text('Veuillez renseigner un numéro de téléphone valide');
+		} else if (formData.message == "") {
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+			$(formMessages).text('Merci de bien vouloir nous expliquer votre projet, nous l\'étudierons avec soin');
+		} else {
+			// Send the mail to the client
+			let msg = `	<h1>Nouveau projet depuis habidom.fr</h1>
+						<p>Merci de ne pas répondre directement à ce mail</p>
+						<p>Si vous avez le moindre soucis, adressez-vous à contact@habidom.fr</p>
+						<h2>Informations du client</h2>
+						<ul>
+							<li>Nom : ${formData.name}</li>
+							<li>Entreprise : ${formData.society}</li>
+							<li>Email : ${formData.mail}</li>
+							<li>Téléphone : ${formData.phone}</li>
+						</ul>
+            			<p>${formData.message}</p>
+						</br>`;
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
+			Email.send({
+				SecureToken: "b2a86528-d4f8-42eb-981a-e69959da32aa",
+				To : [formData.mail, "contact@habidom.fr"],
+				From : "noreply@habidom.fr",
+				Subject : "Message envoyé depuis Habidom.fr",
+				Body : msg,
+			}).then(
+				message => {
+					if (message === "OK") {
+						$(this).trigger("reset");
+						$(formMessages).removeClass('error');
+						$(formMessages).addClass('success');
+						$(formMessages).text('Le message a bien été envoyé, nous vous en avons envoyé une copie');
+					} else {
+						$(formMessages).removeClass('success');
+						$(formMessages).addClass('error');
+						$(formMessages).text('Un problème est survenu. Veuillez réessayer ultérieurement. Si le problème persiste, envoyer un mail directement à contact@habidom.fr ou par téléphone');
+					}
+					$('form button').html(btnContent);
+				}
+			);
+
+		}
+		
+		
+	
+		
 	});
 
 });
+
